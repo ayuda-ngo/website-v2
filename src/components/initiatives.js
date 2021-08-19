5543f8a24873017055a5168e3e77f5628c598fda
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2021-08-19 18:46:18 +0530

    added initiatives section

---
+++ src/components/initiatives.js
@@ -0,0 +1,125 @@
+import React from "react";
+
+// project imports
+import Carousel from "react-spring-3d-carousel";
+import { config } from "react-spring";
+
+
+// assets
+import sudhaar from '../images/initiatives/Sudhaar.png'
+import gyaan from '../images/initiatives/Gyaan.png'
+import nirmaan from '../images/initiatives/Nirmaan.png'
+import gogreen from '../images/initiatives/Go-Green.png'
+import muskaan from '../images/initiatives/Muskaan.png'
+import { ArrowLeftSquareFill, ArrowRightSquareFill } from '@styled-icons/bootstrap'
+
+
+const Initiatives = () => {
+  const [card, setCard] = useState({
+    goToSlide: 0,
+    offsetRadius: 2,
+    showNavigation: true,
+    config: config.slow,
+  });
+
+  const InCards = [
+    {
+      key: 1,
+      content: <img src={sudhaar} alt="1" />,
+      link: "https://medium.com/series/ccf401c07ae",
+    },
+    {
+      key: 2,
+      content: <img src={gyaan} alt="2" />,
+      link: "https://medium.com/series/40893625143b",
+    },
+    {
+      key: 3,
+      content: <img src={nirmaan} alt="3" />,
+      link: "https://medium.com/series/fb1492a32180",
+    },
+    {
+      key: 4,
+      content: <img src={gogreen} alt="4" />,
+      link: "https://medium.com/series/46e8ab47fa44",
+    },
+    {
+      key: 5,
+      content: <img src={muskaan} alt="5" />,
+      link: "https://medium.com/series/48c090b2a22f",
+    },
+    {
+      key: 6,
+      content: <img src={sudhaar} alt="1" />,
+      link: "https://medium.com/series/ccf401c07ae",
+    },
+    {
+      key: 7,
+      content: <img src={gyaan} alt="2" />,
+      link: "https://medium.com/series/40893625143b",
+    },
+    {
+      key: 8,
+      content: <img src={nirmaan} alt="3" />,
+      link: "https://medium.com/series/fb1492a32180",
+    },
+    {
+      key: 9,
+      content: <img src={gogreen} alt="4" />,
+      link: "https://medium.com/series/46e8ab47fa44",
+    },
+    {
+      key: 10,
+      content: <img src={muskaan} alt="5" />,
+      link: "https://medium.com/series/48c090b2a22f",
+    },
+  ].map((slide, index) => {
+    return {
+      ...slide,
+      onClick: () => {
+        if (index === card.goToSlide) window.open(slide.link);
+        setCard({ ...card, goToSlide: index });
+      },
+    };
+  });
+
+  const onRight = () => {
+    setCard({ ...card, goToSlide: card.goToSlide + 1 });
+  };
+
+  const onLeft = () => {
+    setCard({ ...card, goToSlide: card.goToSlide - 1 });
+  };
+
+  return (
+    <div className="Initiatives">
+      <br />
+      <br />
+      <div className="title">
+        <h1>
+          <span>OUR</span> INITIATIVES
+        </h1>
+      </div>
+      <div className="initiative-cards">
+        <Carousel
+          slides={InCards}
+          goToSlide={card.goToSlide}
+          offsetRadius={card.offsetRadius}
+          showNavigation={card.showNavigation}
+          animationConfig={card.config}
+        />
+      </div>
+
+      <div className="row arrows">
+        <ArrowLeftSquareFill size="40" onClick={onLeft} className="leftarr" />
+        <ArrowRightSquareFill
+          size="40"
+          onClick={onRight}
+          className="rightarr"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Initiatives;
