4a7adffd4f5522cad842c8afc43b32b6c1e0d932
Akhil Srinivas P <akhilsrinivasp@gmail.com>
2023-11-17 14:07:30 +0530

    Update Board 2023 on branch

---
+++ src/data/board-data/board-data-2023.js
@@ -0,0 +1,109 @@
+// assets
+import pic20231 from "../../images/board/2023/pic2023-1.jpg";
+import pic20232 from "../../images/board/2023/pic2023-2.jpg";
+import pic20233 from "../../images/board/2023/pic2023-3.jpg";
+import pic20234 from "../../images/board/2023/pic2023-4.jpg";
+import pic20235 from "../../images/board/2023/pic2023-5.jpg";
+import pic20236 from "../../images/board/2023/pic2023-6.jpg";
+import pic20237 from "../../images/board/2023/pic2023-7.jpg";
+import pic20238 from "../../images/board/2023/pic2023-8.jpg";
+import pic20239 from "../../images/board/2023/pic2023-9.jpg";
+import pic202310 from "../../images/board/2023/pic2023-10.jpg";
+
+// data
+const y2023 = {
+  title: "2023-2024",
+  data: [
+    {
+      id: "1",
+      img: pic20231,
+      name: "Akhil Srinivas P",
+      designation: "Chairperson",
+      instagram: "https://www.instagram.com/akhilsrinivasp/",
+      linkedin: "https://www.linkedin.com/in/akhilsrinivasp/",
+    },
+     	
+    {
+      id: "2",
+      img: pic20232,
+      name: "Ananya Singh",
+      designation: "Vice Chairperson",
+      instagram: "https://instagram.com/anxisole",
+      linkedin: "https://www.linkedin.com/in/ananyasingh1473",
+    },
+
+    {
+      id: "3",
+      img: pic20233,
+      name: "Girshali Manimaran",
+      designation: "Secretary",
+      instagram: "https://instagram.com/girshali?utm_medium=copy_link",
+      linkedin: "http://www.linkedin.com/in/girshali-manimaran-371b47226",
+    },
+    	
+    {
+      id: "4",
+      img: pic20234,
+      name: "Chada Lakshmi Mansi",
+      designation: "Co-Secretary",
+      instagram: "https://www.instagram.com/eyeris96/",
+      linkedin: "https://www.linkedin.com/in/lakshmi-mansi-chada-791789222/",
+    },
+    
+    {
+      id: "5",
+      img: pic20235,
+      name: "Mahek Gupta",
+      designation: "Events Head",
+      instagram: "https://instagram.com/btwitsmahek._",
+      linkedin: "https://www.linkedin.com/in/mahek-gupta-125746230",
+    },
+
+    {
+      id: "6",
+      img: pic20236,
+      name: "Pranavi Bhupalam",
+      designation: "Outreach Head",
+      instagram: "https://www.instagram.com/pranahhhvi/",
+      linkedin: "https://www.linkedin.com/in/pranavi-bhupalam-452b61288/",
+    },
+    
+    {
+      id: "9",
+      img: pic20239,
+      name: "Melwin Biju Kalayil",
+      designation: "Publicity & Marketing Head",
+      instagram: "https://instagram.com/melwinkalayil",
+      linkedin: "https://www.linkedin.com/in/melwinkalayil/",
+    },
+
+    {
+      id: "10",
+      img: pic202310,
+      name: "Parvathi P Pillai",
+      designation: "Design Head",
+      instagram: "https://instagram.com/parvathipillaiii",
+      linkedin: "https://www.linkedin.com/in/parvathippillai",
+    },
+
+    {
+      id: "7",
+      img: pic20237,
+      name: "Satwika",
+      designation: "Editorial Head",
+      instagram: "https://www.instagram.com/inlet_out97/",
+      linkedin: "https://www.linkedin.com/in/satwikab/",
+    },
+    
+    {
+      id: "8",
+      img: pic20238,
+      name: "Vaibhav Pathak",
+      designation: "Finance Head",
+      instagram: "https://www.instagram.com/thevaibhavpathak/",
+      linkedin: "https://www.linkedin.com/in/thevaibhavpathak/",
+    },
+  ],
+};
+
+export default y2023;
